8679416171fbc9b804cf7d08881c2d29bb5ba7ec
Zachary <zstimmel@gmail.com>
2017-11-17 16:02:39 -0500

    console

---
+++ src/main/java/io/zipcoder/casino/Console/Console.java
@@ -0,0 +1,30 @@
+package io.zipcoder.casino.Console;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Console {
+
+    public static String getString(String prompt) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(prompt);
+        String userinput = input.nextLine();
+        return userinput;
+    }
+
+    public static double getDouble(String prompt) {
+
+        do {
+            try {
+                Scanner input = new Scanner(System.in);
+                System.out.println(prompt);
+                double userinput = input.nextDouble();
+                return userinput;
+            } catch (IllegalArgumentException | InputMismatchException e) {
+                System.out.println("INVALID AMOUNT\n");
+                continue;
+            }
+        } while (true);
+    }
+
+}
