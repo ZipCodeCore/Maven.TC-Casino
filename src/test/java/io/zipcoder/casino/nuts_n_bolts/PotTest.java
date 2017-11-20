36bd98413b90596718496cc9c6556ea0daa4c00e
timothy <timothy@zipcoders-MacBook-Pro.local>
2017-11-20 17:28:57 -0500

    Pot and PotTest

--- src/test/java/io/zipcoder/casino/nuts_n_bolts/PotTest.java
+++ src/test/java/io/zipcoder/casino/nuts_n_bolts/PotTest.java
@@ -1,4 +1,49 @@
 package io.zipcoder.casino.nuts_n_bolts;
 
+import org.junit.Test;
+import org.junit.Assert;
+import java.util.Random;
+
 public class PotTest {
+
+    private Random rando = new Random();
+
+    @Test
+    public void testAddToPotAndGetMoney(){
+        Pot testPot = new Pot();
+        Double amountToAdd = rando.nextDouble();
+        Double actual;
+        testPot.addToPot(amountToAdd);
+        actual=testPot.getMoney();
+        Assert.assertEquals(amountToAdd, actual, 0.0001);
+    }
+
+    @Test
+    public void testTakeOutOfPot(){
+        Pot testPot = new Pot();
+        Double amountToRemove = rando.nextDouble();
+        Double actual;
+        Double expected;
+
+        testPot.addToPot(rando.nextDouble());
+        if (amountToRemove<testPot.getMoney())
+            expected = amountToRemove;
+        else
+            expected=0.0;
+        actual=testPot.takeOutOfPot(amountToRemove);
+        Assert.assertEquals(expected, actual, 0.0001);
+    }
+
+    @Test
+    public void testTakeAllOutOfPot(){
+        Pot testPot = new Pot();
+        testPot.addToPot(rando.nextDouble());
+        Double expected=testPot.getMoney();
+        Double actual = testPot.takeAllOutOfPot();
+
+        Assert.assertEquals(expected, actual, 0.0001);
+        Assert.assertEquals(0.0, testPot.getMoney(), 0.0001);
+    }
+
+
 }
