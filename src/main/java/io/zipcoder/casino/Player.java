de8d1a6700b8265fbacc93dfe78451259b12062c
graham <graham@zipcoders-MacBook-Pro.local>
2017-11-20 10:40:32 -0500

    player class

---
+++ src/main/java/io/zipcoder/casino/Player.java
@@ -0,0 +1,48 @@
+package io.zipcoder.casino;
+
+import java.util.ArrayList;
+
+public class Player {
+    public String name;
+    public double balance;
+    public double age;
+    public ArrayList<Card> playerHand = new ArrayList<Card>();
+
+    public Player(String name, double balance, double age){
+        this.name = name;
+        this.balance = balance;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public void setAge(double age) {
+        this.age = age;
+    }
+
+    public ArrayList<Card> getPlayerHand() {
+        return playerHand;
+    }
+
+    public void setPlayerHand(ArrayList<Card> playerHand) {
+        this.playerHand = playerHand;
+    }
+}
