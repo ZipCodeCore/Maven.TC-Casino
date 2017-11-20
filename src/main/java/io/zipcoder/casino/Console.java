a12ebe89243ebf313f841331e88405ceb9e3366d
donald f <donaldf@zipcoders-MacBook-Pro-9.local>
2017-11-20 10:50:34 -0500

    created console

---
+++ src/main/java/io/zipcoder/casino/Console.java
@@ -0,0 +1,46 @@
+package io.zipcoder.casino;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Console {
+    public static String getUserInputString(String promptUser){
+        do {
+
+            try {
+                System.out.println(promptUser);
+                String userInput = new Scanner(System.in).nextLine();
+                return userInput;
+            } catch (InputMismatchException ime){
+                System.out.println("Invalid input, please try again.");
+                continue;
+            }
+        }
+        while (true) ;
+    }
+
+    public static Double getUserInputDouble(String promptUser){
+        do {
+            try {
+                return Double.parseDouble(getUserInputString(promptUser));
+            } catch (NumberFormatException nfe){
+                System.out.println("Invalid response please try again.");
+                continue;
+            }
+        } while (true);
+
+    }
+
+    public static Integer getUserInputInteger(String promptUser){
+        do {
+            try {
+                return Integer.parseInt(getUserInputString(promptUser));
+            } catch (NumberFormatException nfe){
+                System.out.println("Invalid response please try again.");
+                continue;
+            }
+        } while (true);
+
+
+    }
+}
