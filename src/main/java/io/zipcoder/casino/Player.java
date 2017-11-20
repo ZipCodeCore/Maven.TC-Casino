62eaadb4e4942901ca48d56a8bddda7fa5d78849
wesley <wesley@zipcoders-MacBook-Pro-8.local>
2017-11-20 11:31:57 -0500

    add to player

--- src/main/java/io/zipcoder/casino/Player.java
+++ src/main/java/io/zipcoder/casino/Player.java
@@ -47,4 +47,26 @@ public class Player {
     public void setPlayerHand(ArrayList<Card> playerHand) {
         this.playerHand = playerHand;
     }
+
+    public void addCard(Card card){
+        playerHand.add(card);
+    }
+
+    public void clearHand(){
+        playerHand.clear();
+    }
+
+    public String getStringDisplayHand(){
+        String output = "";
+        Card card;
+        for (int i=0; i<playerHand.size(); i++)
+        {
+            card=playerHand.get(i);
+            if (i!=playerHand.size()-1)
+                output += card.getTopCardRepresentation();
+            else
+                output+= card.toString();
+        }
+        return output;
+    }
 }
