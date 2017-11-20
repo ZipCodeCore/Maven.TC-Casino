44fb0ee1c4f2eee863856d5c338facf085d8d8fe
graham <graham@zipcoders-MacBook-Pro.local>
2017-11-20 14:52:28 -0500

    commit to pull abstracts

--- src/main/java/io/zipcoder/casino/GoFish.java
+++ src/main/java/io/zipcoder/casino/GoFish.java
@@ -4,62 +4,94 @@ import io.zipcoder.casino.Console.Console;
 import io.zipcoder.casino.Deck.Card;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GoFish extends CardGame {
 
     private int playerBookCount = 0;
     private int dealerBookCount = 0;
-    Player dealer = new Player("Dealer", 0,6);
+    Player dealer = new Player("Dealer", 0, 6);
 
-    public GoFish(){
+    public GoFish() {
 
     }
 
-    public void goFishStart(){
+    public void goFishStart() {
         deal(casinoplayer, dealer, 7);
-        Console.print(casinoplayer.getStringDisplayHand());
-        getCard("Give me all your: ");
-
+        playerTurn();
 
 
     }
 
-    private void playerAsk(){
-
+    private void playerTurn() {
+        boolean playing = true;
+        while (playing) {
+            Console.print(casinoplayer.getStringDisplayHand());
+            Card askCard = getCard("Give me all your: ");
+            if (isCardInHand(askCard, dealer.getPlayerHand())) {
+                dealer.getPlayerHand().remove(askCard);
+                casinoplayer.addCard(askCard);
+            } else {
+                Console.print("GO FISH!");
+                giveCard(casinoplayer);
+                playing = false;
+            }
+        }
     }
 
-    private boolean dealerAsk(){
+
+    private boolean dealerAsk() {
         return false;
     }
 
-//    private boolean isCardInHand(Card askCard, ArrayList<Card> hand){
-//        for(Card card:hand){
-//            if(card.fishValue = askCard.fishValue){
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
+    private boolean isCardInHand(Card askCard, ArrayList<Card> hand) {
+        for (Card card : hand) {
+            if (card.getValue().equals(askCard.getValue())) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-//    private Card getCard(String prompt){
-//        Console.print(prompt);
-//
-//
-//    }
+    private Card getCard(String prompt) {
+        String input = Console.getString("Give me all your: ");
 
-//    private void removeCard(Card card, ArrayList<Card> hand){
-//
-//    }
-//    private void compareBooks(){
-//        if(playerBooks > dealerBooks){
-//            System.out.println("You win!");
-//        }
-//        else if(playerBooks < dealerBooks){
-//            System.out.println("You lose!");
-//        }
-//        else{
-//            System.out.println("Tie!");
-//        }
+        }
+
+        return null;
+
+    }
+
+    private void removeCard(Card card, ArrayList<Card> hand) {
+
+    }
+
+    private Card checkForBooks(Player player) {
+        ArrayList<Card> hand = player.getPlayerHand();
+        for (Card card1 : hand) {
+            int num = 0;
+            for (Card card2 : hand)
+                if (card1 == card2)
+                    num++;
+            if (num == 4) {
+                for (int i = 0; i < 4; i++)
+                    hand.remove(card1);
+                //ADD TO PLAYER BOOK COUNT
+                return card1;
+            }
+        }
+        return null;
+
+    }
+
+    private void compareBooks() {
+        if (playerBookCount > dealerBookCount) {
+            System.out.println("You win!");
+        } else if (playerBookCount < dealerBookCount) {
+            System.out.println("You lose!");
+        } else {
+            System.out.println("Tie!");
+        }
     }
 
 
---
+++ src/test/java/io/zipcoder/casino/GoFishTest.java
@@ -0,0 +1,10 @@
+package io.zipcoder.casino;
+
+import org.junit.Test;
+
+public class GoFishTest {
+
+
+    @Test
+    public void
+}
