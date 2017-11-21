b7a8a0032867959d30ec16ac49a322dd35622318
merin j <merinj@zipcoders-MacBook-Pro-7.local>
2017-11-21 17:25:51 -0500

    1st commit

---
+++ src/main/java/io/zipcoder/casino/Console.java
@@ -0,0 +1,23 @@
+package io.zipcoder.casino;
+
+import java.util.Scanner;
+
+public class Console {
+    public static String getStringInput(String prompt) {
+        System.out.println(prompt);
+        Scanner scanner= new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        return userInput;
+    }
+
+    public static Double getDoubleInput(String prompt) {
+        String userInputString = getStringInput(prompt);
+        double userInputDouble = Double.parseDouble(userInputString);
+        return userInputDouble;
+    }
+    public static Integer getIntegerInput(String prompt) {
+        String userInputString = getStringInput(prompt);
+        double userInputInteger = Integer.parseInt(userInputString);
+        return (int)userInputInteger;
+    }
+}
