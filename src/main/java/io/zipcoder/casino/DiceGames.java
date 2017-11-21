77ef91b8ae31d8b342816cbb6a224fcbef26fc3b
donald f <donaldf@zipcoders-MacBook-Pro-9.local>
2017-11-21 13:06:44 -0500

    change some stuff for collabo stuff

--- src/main/java/io/zipcoder/casino/DiceGames.java
+++ src/main/java/io/zipcoder/casino/DiceGames.java
@@ -1,4 +1,39 @@
 package io.zipcoder.casino;
 
 public class DiceGames {
+
+     public void chooseDiceGame(){
+         int userInput = Console.getUserInputInteger("Which dice game 1 or 2.");
+         if(userInput == 1){
+             startCraps;
+         }
+     } if else (userInput == 2){
+
+         }else{
+             repeatQuestion;}
+
+    public void startCraps(){
+        do{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            String userInput = Console.getUserInputString("Do wyou wnat to continue yes or no?");
+        }while (userInput.equalsIgnoreCase("yes"));
+    }
 }
