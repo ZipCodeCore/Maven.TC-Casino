82a8828db092b21b9289abd84ecae7ba9acf25f4
pavel <Pavelparfenov112@gmail.com>
2017-11-20 19:09:18 -0500

    "Added separate base hand"

--- src/test/java/io/zipcoder/casino/nuts_n_bolts/cards/HandTest.java
+++ src/test/java/io/zipcoder/casino/nuts_n_bolts/cards/HandTest.java
@@ -1,4 +1,45 @@
 package io.zipcoder.casino.nuts_n_bolts.cards;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class HandTest {
+
+    @Test
+    public void handShouldBeEmptyTest(){
+        Hand hand = new Hand();
+
+        Assert.assertTrue(hand.isHandEmpty());
+    }
+
+    @Test
+    public void addAndhandShouldNotBeEmptyTest(){
+        Hand hand = new Hand();
+        hand.addCard(new PlayingCard(PlayingSuit.HEART, PlayingValue.QUEEN));
+
+        Assert.assertFalse(hand.isHandEmpty());
+    }
+
+    @Test
+    public void addAndRemoveTest(){
+        Hand hand = new Hand();
+        hand.addCard(new PlayingCard(PlayingSuit.HEART, PlayingValue.QUEEN));
+        Assert.assertFalse(hand.isHandEmpty());
+
+        hand.removeCard(hand.getAllCards().get(0));
+        Assert.assertTrue(hand.isHandEmpty());
+    }
+
+    @Test
+    public void addAndGetTest(){
+        Hand hand = new Hand();
+        PlayingCard expectedCard = new PlayingCard(PlayingSuit.HEART, PlayingValue.QUEEN);
+        hand.addCard(expectedCard);
+
+        PlayingCard actualCard = hand.getCard(expectedCard);
+        Assert.assertTrue(hand.isHandEmpty());
+
+        Assert.assertEquals(expectedCard, actualCard);
+    }
+
 }
