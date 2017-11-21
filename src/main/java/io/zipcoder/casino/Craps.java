c1d65a1597df90a87046c4445fc0c2e380cfbc1c
jeffrey <jeffrey@zipcoders-MacBook-Pro-3.local>
2017-11-21 16:13:07 -0500

    testing Craps update

--- src/main/java/io/zipcoder/casino/Craps.java
+++ src/main/java/io/zipcoder/casino/Craps.java
@@ -1,4 +1,14 @@
 package io.zipcoder.casino;
+import java.util.*;
 
 public class Craps extends DiceGames {
+
+    public static int roll() {
+        Random diceRoll = new Random();
+        int dice1 = diceRoll.nextInt(6) + 1;
+        int dice2 = diceRoll.nextInt(6) + 1;
+        System.out.println("\n dice1 = " + dice1 + " dice2 = " + dice2);
+        return dice1 + dice2;
+    }
+
 }
