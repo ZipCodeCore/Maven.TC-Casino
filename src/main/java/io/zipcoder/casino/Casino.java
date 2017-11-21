dc0646880cd2e6255e479577c9f9fb34f16d1373
rob <rob@zipcoders-MacBook-Pro.local>
2017-11-21 16:44:22 -0500

    Enums, Card done, deck started

--- src/main/java/io/zipcoder/casino/Casino.java
+++ src/main/java/io/zipcoder/casino/Casino.java
@@ -3,8 +3,6 @@ package io.zipcoder.casino;
 
 public class Casino {
 
-    //You are a fucking dick
-    //Your a fucking dick too
 
 
 }
---
+++ src/main/java/io/zipcoder/casino/Value.java
@@ -0,0 +1,7 @@
+package io.zipcoder.casino;
+
+public enum Value {
+
+    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+
+}
