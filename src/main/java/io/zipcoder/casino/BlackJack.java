77f1a2651e030527c9b9f51880b9cd7c76de4bf8
rob <rob@zipcoders-MacBook-Pro.local>
2017-11-21 21:14:22 -0500

    I originally made my deck tests in CasinoTest, fixed it; passed double in BlackJack so the program can be started from the Casino main

--- src/main/java/io/zipcoder/casino/BlackJack.java
+++ src/main/java/io/zipcoder/casino/BlackJack.java
@@ -1,7 +1,7 @@
 package io.zipcoder.casino;
 
 public class BlackJack extends CardGame{
-    Player dealer = new CardPlayer("Computer");
+    Player dealer = new CardPlayer("Computer", 100.00);
     public void startGame(Player player){
 
     }
