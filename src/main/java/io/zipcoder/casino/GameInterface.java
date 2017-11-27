cdc078d89404f0a972ad0a5ae43275126b855097
aaron <aaronherion111@gmail.com>
2017-11-27 01:25:21 -0500

    Blackjack updates - nearly complete - looping turns and prompts fully functional but logic to determine a winning or losing condition is not working so game never ends

--- src/main/java/io/zipcoder/casino/GameInterface.java
+++ src/main/java/io/zipcoder/casino/GameInterface.java
@@ -8,19 +8,20 @@ public class GameInterface {
 
         String playAgain;
 
-        do {Craps craps = new Craps();
+        do {
+            Craps craps = new Craps();
 
             int numberOfPlayer = Console.getUserInputInteger("Welcome to the craps table. How many people are playing?");
             //creates players
-            for(int i = 1; i <= numberOfPlayer; i++){
+            for (int i = 1; i <= numberOfPlayer; i++) {
                 craps.createOnePlayer(Console.getUserInputString("Player name:"), Console.getUserInputDouble("How much money would you like to bring to the table?"));
             }
 
-             //get players bets
-            for(int b = 0; b <= numberOfPlayer; b++){
+            //get players bets
+            for (int b = 0; b <= numberOfPlayer; b++) {
                 System.out.println(craps.crapsPlayers.get(b).getName());
                 Double playerBet = craps.crapsPlayers.get(b).bet(Console.getUserInputDouble(" please place bet"));
-                System.out.println(craps.crapsPlayers.get(b).getName()+" you bet " + playerBet);
+                System.out.println(craps.crapsPlayers.get(b).getName() + " you bet " + playerBet);
             }
 
 
@@ -36,11 +37,6 @@ public class GameInterface {
 //           do{
 //               int firstRol = craps.firstRoll(craps.crapsPlayers.get(0));
 //            }while(firstRol != 1 || 2);
-
-
-
-
-
 
 
             playAgain = Console.getUserInputString("Do you want to continue playing?");
@@ -71,31 +67,85 @@ public class GameInterface {
             System.out.println("The dealer's card was: " + BlackjackPlayer.viewHand(blackjackPlayer));
             BlackJack.endTurn();
             String playersTurn = "yes";
+            String dealersTurn = "no";
 
-            while (playersTurn.equals("yes")) {
-                BlackJack.giveCardToPlayer();
-                BlackJack.giveCardToPlayer();
-                System.out.println("Your card was: " + BlackjackPlayer.viewHand(blackjackPlayer));
-                String hitOrStay = Console.getUserInputString("Do you want to stay? Yes or no...");
+            while (playersTurn.equals("yes") && dealersTurn.equals("no")) {
 
-                if (hitOrStay.equalsIgnoreCase("yes"))
-                    BlackjackPlayer.hitStay("yes");
-                while (hitOrStay.equalsIgnoreCase("no")) {
-                    BlackjackPlayer.hitStay("no");
-                    System.out.println("Your card was: " + BlackjackPlayer.viewHand(blackjackPlayer));
-                    String hitOrStayAgain = Console.getUserInputString("Do you want to stay? Yes or no...");
-                    if (hitOrStayAgain.equalsIgnoreCase("yes"))
-                    playersTurn = "no";
-                    break;
+                while (playersTurn.equals("yes")) {
+                    BlackJack.giveCardToPlayer();
+                    BlackJack.giveCardToPlayer();
+                    System.out.println("Your hand's total is: " + BlackjackPlayer.viewHand(blackjackPlayer));
+                    String hitOrStay = Console.getUserInputString("Do you want to stay? Yes or no...");
 
+                    if (hitOrStay.equalsIgnoreCase("yes")) {
+                        BlackjackPlayer.hitStay("yes");
+                        BlackJack.endTurn();
+                        dealersTurn = "yes";
+                        playersTurn = "no";
+                        break;
+                    }
+                    while (hitOrStay.equalsIgnoreCase("no")) {
+                        BlackjackPlayer.hitStay("no");
+                        System.out.println("Your card was: " + BlackjackPlayer.viewHand(blackjackPlayer));
+//                        if (blackjackPlayer.getHandValue() == 21) {
+//                            System.out.println("You win!");
+//                            playAgain = Console.getUserInputString("Do you want to play another game of Blackjack?");
+//                        }
+//                        if (blackjackPlayer.getHandValue() > 21) {
+//                            System.out.println("You busted");
+//                            playAgain = Console.getUserInputString("Do you want to play another game of Blackjack?");
+//                        }
+                        String hitOrStayAgain = Console.getUserInputString("Do you want to stay? Yes or no...");
+                        if (hitOrStayAgain.equalsIgnoreCase("yes")) {
+                            BlackJack.endTurn();
+                            playersTurn = "no";
+                            dealersTurn = "yes";
+                            break;
+                        }
+                    }
                 }
 
+                while (dealersTurn.equals("yes") && playersTurn.equals("no")) {
+
+                    while (dealersTurn.equals("yes")) {
+                        BlackJack.giveCardToPlayer();
+                        System.out.println("The dealer's card was: " + BlackjackPlayer.viewHand(blackjackPlayer));
+                        String hitOrStay = Console.getUserInputString("Does the dealer want to stay? Yes or no...");
+//                        if (blackjackPlayer.getHandValue() == 21) {
+//                            System.out.println("The dealer won!");
+//                            playAgain = Console.getUserInputString("Do you want to play another game of Blackjack?");
+//                        }
+//                        if (blackjackPlayer.getHandValue() > 21) {
+//                            System.out.println("The dealer busted");
+//
+                        if (hitOrStay.equalsIgnoreCase("yes")) {
+                            BlackjackPlayer.hitStay("yes");
+                            BlackJack.endTurn();
+                            dealersTurn = "no";
+                            playersTurn = "yes";
+                            break;
+                        }
+                        while (hitOrStay.equalsIgnoreCase("no")) {
+                            BlackjackPlayer.hitStay("no");
+                            System.out.println("The dealer's card was: " + BlackjackPlayer.viewHand(blackjackPlayer));
+
+                            String hitOrStayAgain = Console.getUserInputString("Does the dealer want to stay? Yes or no...");
+                            if (hitOrStayAgain.equalsIgnoreCase("yes")) {
+                                BlackJack.endTurn();
+                                dealersTurn = "no";
+                                playersTurn = "yes";
+                                break;
+                            }
+                        }
+                    }
+                }
             }
 
             playAgain = Console.getUserInputString("Do you want to play another game of Blackjack?");
             //the below line to retrieve the same player name from previous game if player wants to continue may fail...
             //so may need to rewrite, won't know until we test the game-play from console itself
             blackjackPlayer.getName();
+
         } while (playAgain.equals("no"));
         CardGames cardGames = new CardGames();
         cardGames.chooseGameType();
@@ -110,14 +160,14 @@ public class GameInterface {
         goFishDeck.shuffle();
         String playAgain = null;
 
-        do{
+        do {
             System.out.println("Welcome to the Go Fish table!!!");
 
-            for(int i =0; i <= 1; i++) {
+            for (int i = 0; i <= 1; i++) {
                 goFish.createOnePlayer(Console.getUserInputString("What is your name?"));
             }
 
-            goFish.goFishPlayers.get(0).dealGoFishHand(goFish.goFishPlayers,goFishDeck);
+            goFish.goFishPlayers.get(0).dealGoFishHand(goFish.goFishPlayers, goFishDeck);
 
             goFish.showEverybodysHand();
 
@@ -132,8 +182,8 @@ public class GameInterface {
         CardGames cardGames = new CardGames();
         cardGames.chooseGameType();
 
-        }
-
     }
+
+}
 
 
