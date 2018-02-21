23562d510c219bee5e5164769463a96baf88322b
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-02-21 14:16:21 -0500

    Dice manager tests coming along

--- src/main/java/io/zipcoder/casino/DiceManager.java
+++ src/main/java/io/zipcoder/casino/DiceManager.java
@@ -2,16 +2,35 @@ package io.zipcoder.casino;
 
 public class DiceManager {
 
-    public void rollDice() {
+    // Die faces will be stored in an array
 
+    private Die[] diceArray;
+
+    public DiceManager(int amountOfDice) {
+        this.diceArray = new Die[amountOfDice];
+    }
+
+    public void rollSpecificDice(int index) {
+        diceArray[index].rollDie();
+    }
+
+    public void rollAllDice() {
+        for (int i = 0; i < diceArray.length; ) {
+            rollSpecificDice(i);
+        }
     }
 
     public int[] checkDiceValue() {
-        return new int[]{0};
+        int[] allDieFaces = new int[]{diceArray.length};
+        for (int i = 0; i < diceArray.length; i++) {
+            allDieFaces[i] = diceArray[i].getDieFace().toInt();
+        }
+        return allDieFaces;
     }
 
     public int checkTotalValue() {
         return 0;
     }
+
 
 }
--- src/test/java/io/zipcoder/casino/DiceManagerTest.java
+++ src/test/java/io/zipcoder/casino/DiceManagerTest.java
@@ -1,7 +1,32 @@
 package io.zipcoder.casino;
 
+import org.junit.Test;
+
 public class DiceManagerTest {
 
+    @Test
+    public void amountOfDiceCreatedTest() {
 
+    }
+
+    @Test
+    public void rollAllDiceTest() {
+
+    }
+
+    @Test
+    public void rollSpecificDieTest() {
+        // This test should fail about 1/6 of the time, because a number could still be the same after a roll
+    }
+
+    @Test
+    public void setSpecificDieFaceTest() {
+
+    }
+
+    @Test
+    public void getDiceArrayTest() {
+
+    }
 
 }
