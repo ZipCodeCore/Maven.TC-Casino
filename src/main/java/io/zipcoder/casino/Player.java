517a4a60256091729092d1508226816126368c05
Patrick Glavin <patjglavin@gmail.com>
2018-02-21 14:24:53 -0500

    Player getter, setter and constructor

--- src/main/java/io/zipcoder/casino/Player.java
+++ src/main/java/io/zipcoder/casino/Player.java
@@ -1,4 +1,37 @@
 package io.zipcoder.casino;
 
 public class Player {
+    private String name;
+    private Integer age;
+    private Integer balance;
+
+    public Player(String name, Integer age, Integer balance) {
+        this.name = name;
+        this.age = age;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Integer balance) {
+        this.balance = balance;
+    }
 }
