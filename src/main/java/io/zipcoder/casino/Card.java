546795521f7252dbb9fd8916820ed321a84a2d79
Luis Romero <luisromero@zipcoders-MacBook-Pro-47.local>
2018-02-21 16:51:16 -0500

    fixes to get tests passing

--- src/main/java/io/zipcoder/casino/Card.java
+++ src/main/java/io/zipcoder/casino/Card.java
@@ -1,4 +1,25 @@
 package io.zipcoder.casino;
 
 public class Card {
+
+    private final Rank rank;
+    private final Suit suit;
+
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public String toString() {
+        return rank + " of " + suit;
+    }
+
 }
--- src/main/java/io/zipcoder/casino/Suit.java
+++ src/main/java/io/zipcoder/casino/Suit.java
@@ -1,4 +1,26 @@
 package io.zipcoder.casino;
 
 public enum Suit {
+
+    CLUBS("clubs", "\u2663"),
+    DIAMONDS("diamonds", "\u2666"),
+    HEARTS("hearts", "\u2665"),
+    SPADES("spades", "\u2660");
+
+    final String suitWord;
+    final String suitSymbol;
+
+    Suit(String suitWord, String suitSymbol) {
+        this.suitWord = suitWord;
+        this.suitSymbol = suitSymbol;
+    }
+
+    public String toString() {
+        return this.suitWord;
+    }
+
+    public String toSymbol() {
+        return this.suitSymbol;
+    }
+
 }
