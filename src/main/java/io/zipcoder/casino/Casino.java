ef7a1543fc47aa17df6625eb4feef6fa74779dc4
Karousha Fennimore <karoushafennimore@zipcoders-MacBook-Pro-78.local>
2018-02-21 15:36:01 -0500

    updated

--- src/main/java/io/zipcoder/casino/Casino.java
+++ src/main/java/io/zipcoder/casino/Casino.java
@@ -3,27 +3,41 @@ package io.zipcoder.casino;
 public class Casino {
     private Player player;
     private Game running;
+    public String prompt;
+    int answer;
 
 
     public Casino() {
-
-    }
-    public Player getPlayer() {
-        return player;
-    }
-
-    public void setPlayer(Player player) {
         this.player = player;
     }
 
-    public Game getRunning() {
-        return running;
+    public void mainMenu(){
+        String prompt = "Welcome to High Rollers Club!\n"+
+                "As a welcome gift, here's 500 chips ($2500)\n" +
+                "Please choose a game to play\n(Enter a number for your selection)\n\n" +
+                "\t1. Black Jack\n\t2. Crazy 8's\n\t3. Ceelo\n\t4. Roulette\n\n\t5.EXIT";
+        answer = IOHandler.promptForIntWithMessage(prompt);
     }
 
-    public void setRunning(Game running) {
-        this.running = running;
+    public void enter(){
+        mainMenu();
+        while(answer != 5) { //X is to eXit
+            switch(answer) {
+                case 1:
+                    Blackjack.start() = running;
+                    break;
+                case 2:
+                    CrazyEight.start() = running;
+                    break;
+                case 3:
+                    Ceelo.start() = running;
+                    break;
+                case 4:
+                    Roulette.start() = running;
+                    break;
+
+            }
+        }
     }
-
-
 }
 
--- src/test/java/io/zipcoder/casino/CasinoTest.java
+++ src/test/java/io/zipcoder/casino/CasinoTest.java
@@ -2,4 +2,5 @@ package io.zipcoder.casino;
 
 
 public class CasinoTest {
+
 }
