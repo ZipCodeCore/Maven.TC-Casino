37fc670ca5b22c17b83fa0690648d8cbb4a4df4d
Joshua Wurdemann <joshuawurdemann@zipcoders-MacBook-Pro-9.local>
2018-02-21 16:44:53 -0500

    added Player

---
+++ src/main/java/Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    private Profile someProfile;
+
+    public Player(Profile someProfile) {
+        this.someProfile = someProfile;
+    }
+
+    public void setSomeProfile(Profile someProfile) {
+        this.someProfile = someProfile;
+    }
+}
