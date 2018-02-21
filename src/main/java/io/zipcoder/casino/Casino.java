1e8723f35d8ad6bf391c03124d7aa725987f11c1
Karousha Fennimore <karoushafennimore@zipcoders-MacBook-Pro-78.local>
2018-02-21 14:01:34 -0500

    skeleton CasinoClass

--- src/main/java/io/zipcoder/casino/Casino.java
+++ src/main/java/io/zipcoder/casino/Casino.java
@@ -1,5 +1,29 @@
 package io.zipcoder.casino;
 
-
 public class Casino {
+    private Player player;
+    private Game running;
+
+
+    public Casino() {
+
+    }
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Game getRunning() {
+        return running;
+    }
+
+    public void setRunning(Game running) {
+        this.running = running;
+    }
+
+
 }
+
