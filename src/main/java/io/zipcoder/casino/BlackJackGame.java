49a8546f05766d979454b4c79704629c0a492d6b
Vincent Gasbarro <vincentgasbarro@zipcoders-MacBook-Pro.local>
2018-02-21 17:22:33 -0500

    added BlackJackPlayer and BlackJackGame classes and test classes

---
+++ src/main/java/io/zipcoder/casino/BlackJackGame.java
@@ -0,0 +1,49 @@
+package io.zipcoder.casino;
+
+public class BlackJackGame extends CardGame implements Game<>{
+
+    BlackJackPlayer dealer = new BlackJackPlayer();
+    BlackJackPlayer user = new BlackJackPlayer();
+
+    public void deal () {
+
+    }
+
+    public void hit (Player aPlayer) {
+
+    }
+
+    public void stand() {
+
+    }
+
+    public void round () {
+
+    }
+
+    public void dealerBehavior() {
+
+    }
+
+    public void addPlayer(Player player) {
+
+    }
+
+    public void removePlayer (Player player) {
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void endGame() {
+
+    }
+
+    public String getRules() {
+        return null;
+    }
+
+
+}
---
+++ src/test/java/io/zipcoder/casino/BlackJackGameTest.java
@@ -0,0 +1,58 @@
+package io.zipcoder.casino;
+
+import org.junit.Test;
+
+public class BlackJackGameTest {
+
+    @Test
+    public void dealTest() {
+
+    }
+
+    @Test
+    public void hitTest() {
+
+    }
+
+    @Test
+    public void standTest() {
+
+    }
+
+    @Test
+    public void roundTest() {
+
+    }
+
+    @Test
+    public void dealerBehavior() {
+
+    }
+
+    @Test
+    public void addPlayerTest() {
+
+    }
+
+    @Test
+    public void removePlayerTest() {
+
+    }
+
+    @Test
+    public void startGame() {
+
+    }
+
+    @Test
+    public void endGame() {
+
+    }
+
+    @Test
+    public void getRulesTest() {
+
+    }
+
+
+}
