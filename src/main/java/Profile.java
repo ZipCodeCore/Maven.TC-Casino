2402424522f180b73b3742e67e1159cf7fc31bd3
Joshua Wurdemann <joshuawurdemann@zipcoders-MacBook-Pro-9.local>
2018-02-21 16:27:26 -0500

    added House & Profile

---
+++ src/main/java/Profile.java
@@ -0,0 +1,25 @@
+public class Profile {
+    private String name;
+    private double accountBalance;
+
+    public Profile(String name, double accountBalance) {
+        this.name = name;
+        this.accountBalance = accountBalance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAccountBalance(double accountBalance) {
+        this.accountBalance = accountBalance;
+    }
+}
