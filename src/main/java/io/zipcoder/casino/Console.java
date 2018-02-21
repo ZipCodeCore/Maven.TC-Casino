24b8c3a3a7ccdd8a9a9d48a01917465cdcaa9cd3
Vincent Gasbarro <vincentgasbarro@zipcoders-MacBook-Pro.local>
2018-02-21 16:20:08 -0500

    created Console and ConsoleTest classes

---
+++ src/main/java/io/zipcoder/casino/Console.java
@@ -0,0 +1,34 @@
+package io.zipcoder.casino;
+
+import java.util.Scanner;
+
+public class Console {
+
+    public Console() {
+
+    }
+
+    Scanner casinoScanner = new Scanner(System.in);
+
+    public String getString(String userInput) {
+        return null;
+    }
+
+    public double getDouble(String userInput) {
+        return 0.00;
+    }
+
+    public int getInt(String userInput) {
+        return 0;
+    }
+
+    public void readFile() {
+
+    }
+
+    public String print() {
+        return null;
+    }
+
+
+}
---
+++ src/test/java/io/zipcoder/casino/ConsoleTest.java
@@ -0,0 +1,33 @@
+package io.zipcoder.casino;
+
+import org.junit.Test;
+
+public class ConsoleTest {
+
+    @Test
+    public void getStringTest() {
+
+    }
+
+    @Test
+    public void getDoubleTest() {
+
+    }
+
+    @Test
+    public void getIntTest() {
+
+    }
+
+    @Test
+    public void readFile() {
+
+    }
+
+    @Test
+    public void print() {
+
+    }
+
+
+}
