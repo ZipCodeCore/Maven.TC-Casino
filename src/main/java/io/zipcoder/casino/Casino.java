f9e66e903ea10f138dd900fdd16be4ffd917757e
CWinarski <Winarski70@yahoo.com>
2018-02-21 16:21:14 -0500

    make changes to casino  class

--- src/main/java/io/zipcoder/casino/Casino.java
+++ src/main/java/io/zipcoder/casino/Casino.java
@@ -2,11 +2,21 @@ package io.zipcoder.casino;
 
 
 public class Casino {
+
+    InputOutput inputOutput = new InputOutput();
+
     protected void askForUserName(){
+
+        InputOutput inputOutput = new InputOutput();
+
         System.out.println("Hello Player! What is your name?");
-       // String name = inputOutput.scanForString();
-        // Player player = new Player(name, age);
+        String name = inputOutput.scanForString();
+
+
+        //Player player = new Player(name, age);
        // player.setName(name);
     }
+
+    
 
 }
