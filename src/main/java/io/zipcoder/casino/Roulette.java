3dd4b05227c819e0e543b5a84c245a39ad83541b
ghost1497 <rfrankie04@gmail.com>
2018-02-21 18:09:50 -0500

    added rouletteboard

--- src/main/java/io/zipcoder/casino/Roulette.java
+++ src/main/java/io/zipcoder/casino/Roulette.java
@@ -1,4 +1,15 @@
 package io.zipcoder.casino;
 
 public class Roulette {
+    private Player player;
+    private RouletteBoard rouletteBoard;
+
+    public Roulette(Player player){
+        this.player = player;
+        rouletteBoard = new RouletteBoard();
+    }
+
+    public void play(Player player){
+
+    }
 }
