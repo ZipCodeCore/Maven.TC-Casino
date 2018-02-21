3b9d09e4089693f3707a1ed06172d7bf38d08bad
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-02-21 18:16:43 -0500

    update

---
+++ src/main/java/io/zipcoder/casino/GoFishPlayer.java
@@ -0,0 +1,18 @@
+package io.zipcoder.casino;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GoFishPlayer extends CardPlayer{
+
+    private HashMap<Card, Integer>books;
+
+
+    public GoFishPlayer() {
+    }
+    public void buildBook(ArrayList<Card> inputCards){
+
+    }
+
+
+}
