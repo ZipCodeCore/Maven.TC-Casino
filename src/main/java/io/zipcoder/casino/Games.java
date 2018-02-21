128f89fbbee6072746f2dc19c2cca98602e428ee
Lawrence Wu <lawrencewu@zipcoders-MacBook-Pro-3.local>
2018-02-21 14:20:19 -0500

    cardgames and games and hand(maybe)

---
+++ src/main/java/io/zipcoder/casino/Games.java
@@ -0,0 +1,16 @@
+package io.zipcoder.casino;
+
+public interface Games {
+    public void play(Player){
+
+    }
+    public void quitGame(){
+
+    }
+    private void runWelcome(){
+
+    }
+    public String getRules(){
+
+    }
+}
