c1cddaff6c853ea3efd4954e23cb729313dcec36
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-02-21 12:05:41 -0500

    Die done, working on dice manager

--- src/main/java/io/zipcoder/casino/DiceManager.java
+++ src/main/java/io/zipcoder/casino/DiceManager.java
@@ -2,6 +2,18 @@ package io.zipcoder.casino;
 
 public class DiceManager {
 
-    
+    public void rollDice() {
+
+    }
+
+    public int[] checkDiceValue() {
+
+    }
+
+    public int checkTotalValue() {
+
+    }
+
+
 
 }
