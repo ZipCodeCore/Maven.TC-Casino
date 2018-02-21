583b34609c7ef1be4b67a154e3d679a1609fc19c
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-02-21 17:58:01 -0500

    update

--- src/main/java/io/zipcoder/casino/Hand.java
+++ src/main/java/io/zipcoder/casino/Hand.java
@@ -7,5 +7,21 @@ public class Hand {
     private ArrayList<Card>cards;
 
 
+    public Hand(ArrayList<Card> cards) {
+        this.cards = cards;
+    }
 
+    public void addCard(Card card){
+
+    }
+    public void removeCard(Card card){
+    }
+
+    public void clear(){
+
+    }
+
+    public boolean hasCard(Card card){
+        return true;
+    }
 }
--- src/main/java/io/zipcoder/casino/Player.java
+++ src/main/java/io/zipcoder/casino/Player.java
@@ -3,6 +3,7 @@ package io.zipcoder.casino;
 import io.zipcoder.casino.Profile;
 
 public class Player {
+
     private Profile someProfile;
 
     public Player(Profile someProfile) {
