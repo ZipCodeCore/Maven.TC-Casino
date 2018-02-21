e4c820a0c7198b3c9bec4de8ea10bfc8810e4eb2
CWinarski <Winarski70@yahoo.com>
2018-02-21 15:47:50 -0500

    Made changes to player class

--- src/main/java/io/zipcoder/casino/Player.java
+++ src/main/java/io/zipcoder/casino/Player.java
@@ -1,4 +1,42 @@
 package io.zipcoder.casino;
 
 public class Player {
+
+    String name;
+    Integer age;
+    Integer balance;
+
+    public Player(String name, Integer age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public Player(String name, Integer age, Integer balance) {
+        this.name = name;
+        this.age = age;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    public Integer getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Integer balance) {
+        this.balance = balance;
+    }
+
 }
