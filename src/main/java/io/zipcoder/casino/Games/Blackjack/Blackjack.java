04be7f51c264792d03c05ee0b39d448e3546c703
Patrick Glavin <patjglavin@gmail.com>
2018-02-21 15:03:45 -0500

    eh

--- src/main/java/io/zipcoder/casino/Games/Blackjack/Blackjack.java
+++ src/main/java/io/zipcoder/casino/Games/Blackjack/Blackjack.java
@@ -1,8 +1,11 @@
 package io.zipcoder.casino.Games.Blackjack;
 
+import io.zipcoder.casino.Player;
+
 public class Blackjack {
     public void start(){
+        Player sue = new Player("sue", 30, 1000);
         Dealer bkjkDealer = new Dealer();
-        BlackjackPlayer sue = new BlackjackPlayer();
+        BlackjackPlayer sueBKJK = new BlackjackPlayer(sue);
     }
 }
--- src/main/java/io/zipcoder/casino/InputOutput.java
+++ src/main/java/io/zipcoder/casino/InputOutput.java
@@ -1,4 +1,24 @@
 package io.zipcoder.casino;
 
+import java.util.Scanner;
+
 public class InputOutput {
+    Scanner input = new Scanner(System.in);
+
+    public String scanForString(){
+        String stringReturn = new String();
+        if (input.hasNext()){
+            stringReturn = input.nextLine();
+        }
+        return stringReturn;
+    }
+
+    public Integer scanForInt(){
+        Integer integerReturn = 0;
+        while (!input.hasNextInt()) {
+            System.out.println("Please enter a valid number");
+            integerReturn = input.nextInt();
+        }
+        return integerReturn;
+    }
 }
