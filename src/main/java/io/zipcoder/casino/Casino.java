bded9d0261c57f8106d8d43af7a85227d3fdd085
Karousha Fennimore <karousha@gmail.com>
2018-02-21 17:04:33 -0500

    final

--- src/main/java/io/zipcoder/casino/Casino.java
+++ src/main/java/io/zipcoder/casino/Casino.java
@@ -3,7 +3,6 @@ package io.zipcoder.casino;
 public class Casino {
     private Player player;
     private Game running;
-    public String prompt;
     int answer;
 
 
@@ -13,6 +12,7 @@ public class Casino {
     }
 
     public int getAnswer() {
+
         return answer;
     }
 
@@ -20,29 +20,31 @@ public class Casino {
         String prompt = "Welcome to High Rollers Club!\n"+
                 "As a welcome gift, here's 500 chips ($2500)\n" +
                 "Please choose a game to play\n(Enter a number for your selection)\n\n" +
-                "\t1. Black Jack\n\t2. Crazy 8's\n\t3. Ceelo\n\t4. Roulette\n\n\t5.EXIT";
+                "\t1. Black Jack\n\t2. Crazy 8's\n\t3. Ceelo\n\t4. Roulette\n\t\t5.EXIT";
         answer = IOHandler.promptForIntWithMessage(prompt);
     }
 
     public void enter(){
-        while(answer != 5) { //X is to eXit
+        while(answer != 5) {
             mainMenu();
 
             switch(answer) {
                 case 1:
-                    Blackjack.start() = running;
+                    running = new BlackJack();
                     break;
                 case 2:
-                    CrazyEight.start() = running;
+                    running = new CrazyEights();
                     break;
                 case 3:
-                    Ceelo.start() = running;
+                    running= new Ceelo();
                     break;
                 case 4:
-                    Roulette.start() = running;
+                    running = new Roulette();
                     break;
 
             }
+
+            running.play(player);
         }
     }
 
