d340524dc79460bba8e82b3e47d0a92ceb546614
Joshua Wurdemann <joshuawurdemann@zipcoders-MacBook-Pro-9.local>
2018-02-22 17:49:18 -0500

    update

--- src/main/java/io/zipcoder/casino/Game/cardGame/BLackJack/BlackJackGame.java
+++ src/main/java/io/zipcoder/casino/Game/cardGame/BLackJack/BlackJackGame.java
@@ -13,6 +13,7 @@ public class BlackJackGame extends CardGame implements Game {
 
     public BlackJackGame (BlackJackPlayer player) {
         theHouse = new Profile("Dealer", 0, 0);
+
         dealer = new BlackJackPlayer(theHouse);
         this.blackJackPlayer = player;
     }
