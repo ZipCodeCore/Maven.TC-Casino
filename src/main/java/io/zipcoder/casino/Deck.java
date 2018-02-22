99373d9fc710c1db4d7a1126448093aa18fc8e33
vvmk <vincentmasiello@gmail.com>
2018-02-22 00:25:05 -0500

    add/test CeeLoGamble, small refactors in Deck

--- src/main/java/io/zipcoder/casino/Deck.java
+++ src/main/java/io/zipcoder/casino/Deck.java
@@ -21,25 +21,49 @@ public class Deck {
         deckInPlay = new ArrayList<Card>(fullDeck);
     }
 
+    /**
+     * check for a specific card in the deck (mainly for testing pull/remove)
+     * @param card the card to search for
+     * @return true if the card is still in the deck
+     */
     protected boolean searchForCard(Card card) {
         return deckInPlay.contains(card);
     }
 
+    /**
+     * remove a card from the deck, abstracted from pull()
+     * @param card the card to remove
+     */
     private void removeFromDeck(Card card) {
         deckInPlay.remove(card);
     }
 
+    /**
+     * draw and return a single card. use Random to simulate a shuffled deck.
+     * If trying to pull a card from an empty deck, returns null
+     * TODO: discuss
+     * @return a card from a pseudo-randomly chosen index
+     */
     public Card pull() {
+        if (isEmpty())
+           return null;
+
         Card c = deckInPlay.get(new Random().nextInt(getCardsLeft()));
         removeFromDeck(c);
         return c;
     }
 
-    public Card[] pull(int desiredCards) {
-       desiredCards = (desiredCards > getCardsLeft()) ? getCardsLeft() : desiredCards;
+    /**
+     * Attempt to remove numberOfCards from the deck, if there aren't that many left, get them all
+     * TODO: discuss
+     * @param numberOfCards how many cards to remove
+     * @return an array containing the pulled cards
+     */
+    public Card[] pull(int numberOfCards) {
+       numberOfCards = (numberOfCards > getCardsLeft()) ? getCardsLeft() : numberOfCards;
 
-        Card[] ret = new Card[desiredCards];
-        for (int i=0;i<desiredCards;i++)
+        Card[] ret = new Card[numberOfCards];
+        for (int i=0;i<numberOfCards;i++)
             ret[i] = pull();
         return ret;
     }
@@ -61,6 +85,9 @@ public class Deck {
         return getCardsLeft() == 0;
     }
 
+    /**
+     * Build the fresh deck from enums inside Card.java
+     */
     private void buildDeck() {
         for (Card.Suit suit : Card.Suit.values()) {
             for (Card.Rank rank : Card.Rank.values()) {
