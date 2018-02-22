5950404511ea7fcaceaf30fc14f84a0a1af540fe
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-02-22 11:28:41 -0500

    commit

--- src/test/java/io/zipcoder/casinotest/test/GameTest/cardGameTest/GoFishTest/GoFishPlayerTest.java
+++ src/test/java/io/zipcoder/casinotest/test/GameTest/cardGameTest/GoFishTest/GoFishPlayerTest.java
@@ -2,7 +2,7 @@ package io.zipcoder.casinotest.test.GameTest.cardGameTest.GoFishTest;
 
 import org.junit.Before;
 
-public class GoFishTestTest {
+public class GoFishPlayerTest {
 
     @Before
     public void setUp() {
