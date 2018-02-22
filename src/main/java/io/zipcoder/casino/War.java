3c041c0827424e45812171b75bfb390d2e6fd21b
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-02-22 11:13:48 -0500

    War and WarTest started up, THIS VERSION WILL NOT COMPILE DONT USE IT OK

--- src/main/java/io/zipcoder/casino/War.java
+++ src/main/java/io/zipcoder/casino/War.java
@@ -1,4 +1,19 @@
 package io.zipcoder.casino;
 
 public class War {
+
+    Deck originalGameDeck = new Deck();
+    originalGameDeck.shuffleDeck();
+    Deck playerOnePlayedCard = new Deck(); // For the card(s) that get played by player one
+    Deck playerTwoPlayedCard = new Deck(); // For the card(s) that get played by player two
+    // Deck gets 52 cards, gets shuffled, and is split into both player decks
+
+    // In final version of casino, this psvm will be removed because the game will run from the main app
+    // Logic here:
+    public static void main(String[] args) {
+
+    }
+
+    // Will need for loop to iterate through both playedCard ArrayLists to return cards to winningPLayerDeck.add(i)
+
 }
--- src/test/java/io/zipcoder/casino/WarTest.java
+++ src/test/java/io/zipcoder/casino/WarTest.java
@@ -1,4 +1,14 @@
 package io.zipcoder.casino;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class WarTest {
+
+    @Test
+    public void checkPlayerDecks() {
+        War warGame = new War();
+
+    }
+
 }
