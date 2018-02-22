8e9a2ab72961361830331d024d1295c5143ccd0a
Anthony Jordan <anthonyjordan@zipcoders-MacBook-Pro-10.local>
2018-02-22 11:15:18 -0500

    Commiting to pull deck

--- src/test/java/io/zipcoder/casino/WalletTest.java
+++ src/test/java/io/zipcoder/casino/WalletTest.java
@@ -1,4 +1,26 @@
 package io.zipcoder.casino;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class WalletTest {
+
+    @Test
+    public void addChipsTest() {
+        Wallet testWallet = new Wallet();
+        int expected = 500;
+        testWallet.addChips(expected);
+        int actual = testWallet.checkChipAmount();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void removeChipsTest() {
+        Wallet testWallet = new Wallet();
+        testWallet.addChips(500);
+        testWallet.removeChips(200);
+        int expected = 300;
+        int actual = testWallet.checkChipAmount();
+        Assert.assertEquals(actual, expected);
+    }
 }
