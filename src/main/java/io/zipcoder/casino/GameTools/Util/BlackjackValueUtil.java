6c4536da3a283af6bef533a2eb75e9e3b19c96dc
Patrick Glavin <patjglavin@gmail.com>
2018-02-23 15:02:36 -0500

    make foundation for util

---
+++ src/main/java/io/zipcoder/casino/GameTools/Util/BlackjackValueUtil.java
@@ -0,0 +1,12 @@
+package io.zipcoder.casino.GameTools.Deck.Util;
+
+import io.zipcoder.casino.GameTools.Deck.Card;
+import io.zipcoder.casino.GameTools.Deck.Rank;
+import io.zipcoder.casino.GameTools.Deck.Suit;
+import io.zipcoder.casino.GameTools.Deck.Deck;
+
+public class BlackjackValueUtil {
+    public static int rankParse(){
+        return 0;
+    }
+}
