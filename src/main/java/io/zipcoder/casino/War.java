b7c04237143ec2aae5d940272e5e0dc52ab5b738
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-02-23 13:55:07 -0500

    Big war updates, almost playable

--- src/main/java/io/zipcoder/casino/War.java
+++ src/main/java/io/zipcoder/casino/War.java
@@ -1,21 +1,84 @@
 package io.zipcoder.casino;
 
-public class War {
+import java.util.*;
 
-    Deck originalGameDeck = new Deck();
-    originalGameDeck.shuffleDeck();
-    Deck playerOnePlayedCard = new Deck(); // For the card(s) that get played by player one
-    Deck playerTwoPlayedCard = new Deck(); // For the card(s) that get played by player two
-    // Deck gets 52 cards, gets shuffled, and is split into both player decks
+public class War extends Game implements GameInterface, CardGameInterface {
 
-    // In final version of casino, this psvm will be removed because the game will run from the main app
-    // Logic here:
-    public static void main(String[] args) {
+    private Dealer dealer;
+    private Person player;
+    private ArrayList<Card> playerPlayedCards = new ArrayList<Card>();
+    private ArrayList<Card> dealerPlayedCards = new ArrayList<Card>();
+
+    public War(Person player) {
+        this.player = player;
+    }
+
+    public void start() {
+        dealer.getHand().shuffleHand();
+        player.getHand().clearHand();
+        dealCards();
+        beginRound();
+    }
+
+    public void beginRound() {
+        // Upon player input, player1playedCard calls player1hand.draw() and so does player 2 for their playedCard
+        // Ask compareCards for the winner
+        // Based on winner, either call player1wins method or player2wins method or iDeclareWar method
+        // Check length of both players decks and if one == 52, call end
 
     }
 
-    // Will need for loop to iterate through both playedCard ArrayLists to return cards to winningPLayerDeck.add(i)
+    public int compareCards(Card p1card, Card p2card) {
+        if (p1card.getRank().toInt() == p2card.getRank().toInt()) {
+            iDeclareWar();
+        } else if (p1card.getRank().toInt() > p2card.getRank().toInt()) {
+            return 1;
+        } else {return 2;}
+        return 0;
+    }
 
+    public void playerOneWins() {
+        // Uses playedCard.draw on both played card decks until they're empty to return all card objects to player1s deck
+    }
 
+    public void playerTwoWins() {
+        // Uses playedCard.draw on both played card decks until they're empty to return all card objects to player2s deck
+    }
+
+    public void iDeclareWar() {
+        int amountOfPlayerAvailibleCards = checkNumberOfCards(player.getHand());
+        int amountOfDealerAvailibleCards = checkNumberOfCards(dealer.getHand());
+        if (amountOfPlayerAvailibleCards < 4) {
+            for (int i = 0; i < amountOfPlayerAvailibleCards; i++) {
+                playerPlayedCards.add(player.getHand().getHandArrayList().get(i));
+            }
+        } else {
+            for (int i = 0; i < 4; i++) {
+                playerPlayedCards.add(player.getHand().getHandArrayList().get(i));
+            }
+        }
+        if (amountOfDealerAvailibleCards < 4) {
+            for (int i = 0; i < amountOfDealerAvailibleCards; i++) {
+                dealerPlayedCards.add(dealer.getHand().getHandArrayList().get(i));
+            }
+        } else {
+            for (int i = 0; i < 4; i++) {
+                dealerPlayedCards.add(dealer.getHand().getHandArrayList().get(i));
+            }
+        }
+    }
+
+    public void dealCards() {
+        for (int i = dealer.getHand().getHandArrayList().size()-1; i >= (dealer.getHand().getHandArrayList().size())/2; i--) {
+            player.getHand().getHandArrayList().add(dealer.getHand().getHandArrayList().remove(i));
+        }
+    }
+
+    public void end() {
+        // "And the winner is " + winner
+    }
+    public int checkNumberOfCards(Hand handToCheck) {
+        return handToCheck.getHandArrayList().size();
+    }
 
 }
--- src/test/java/io/zipcoder/casino/WarTest.java
+++ src/test/java/io/zipcoder/casino/WarTest.java
@@ -3,12 +3,55 @@ package io.zipcoder.casino;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class WarTest {
 
     @Test
-    public void checkPlayerDecks() {
-        War warGame = new War();
+    public void checkNumberOfCards() {
+        Person player = new Person();
+        War warGame = new War(player);
+        Hand hand = new Hand();
+        hand.getHandArrayList().add(new Card(Rank.DEUCE, Suit.CLUBS));
+        hand.getHandArrayList().add(new Card(Rank.NINE, Suit.HEARTS));
+        int expected = 2;
+        int actual = warGame.checkNumberOfCards(hand);
+        Assert.assertEquals(expected, actual);
+    }
 
+    @Test
+    public void compareCardsTest() {
+        Person player = new Person();
+        War warGame = new War(player);
+        Card smallCard = new Card(Rank.THREE, Suit.DIAMONDS);
+        Card bigCard = new Card(Rank.SEVEN, Suit.DIAMONDS);
+        int expected = 2;
+        int actual = warGame.compareCards(smallCard, bigCard);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void compareCardsTest2() {
+        Person player = new Person();
+        War warGame = new War(player);
+        Card smallCard = new Card(Rank.EIGHT, Suit.DIAMONDS);
+        Card bigCard = new Card(Rank.DEUCE, Suit.DIAMONDS);
+        int expected = 1;
+        int actual = warGame.compareCards(smallCard, bigCard);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void dealCardsTest() {
+        Person player = new Person();
+        War warGame = new War(player);
+        warGame.dealCards();
+    }
+
+    @Test
+    public void iDeclareWarTest() {
+        Person player = new Person();
+        War warGame = new War(player);
     }
 
 }
