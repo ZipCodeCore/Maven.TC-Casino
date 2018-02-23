3ca5474fe660d6a4503f7e2c2afca6861763d9cd
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-02-23 08:35:52 -0500

    commit

--- src/main/java/io/zipcoder/casino/Game/cardGame/GoFIsh/GoFish.java
+++ src/main/java/io/zipcoder/casino/Game/cardGame/GoFIsh/GoFish.java
@@ -8,55 +8,87 @@ import io.zipcoder.casino.Game.cardGame.utilities.Deck;
 import io.zipcoder.casino.Player;
 import io.zipcoder.casino.Profile;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Map;
-import java.util.Stack;
+import java.util.*;
 
-public class GoFish extends CardGame implements Game{
+public class GoFish extends CardGame implements Game {
 
 
     private Player goFishPlayer;
     private Player dealer;
     private Profile playerProfile;
     private Profile dealerProfile;
-    ArrayList<Card>dealerCards= new ArrayList<Card>();
-    ArrayList<Card>playerCards= new ArrayList<Card>();
-    private Deck newDeck;
+    Map<Player,ArrayList<Card>>cardsAtHand=new HashMap<>();
+    ArrayList<Card> dealerCards = new ArrayList<Card>();
+    ArrayList<Card> playerCards = new ArrayList<Card>();
 
-    public GoFish(GoFishPlayer goFishPlayer){
-        dealerProfile = new Profile("DealerName",0,0);
-        dealer= new GoFishPlayer(dealerProfile);
+
+    public GoFish(GoFishPlayer goFishPlayer) {
+        dealerProfile = new Profile("DealerName", 0, 0);
+        dealer = new GoFishPlayer(dealerProfile);
         this.goFishPlayer = goFishPlayer;
+
     }
 
     public GoFish() {
 
     }
 
-    public void deal( Deck deckOfCards, Player player1,Player dealer){
+    public void deal(Deck deckOfCards, Player player1, Player dealer) {
+
+        deckOfCards.fillDeck();
         deckOfCards.shuffle();
-        if(deckOfCards.countRemainingCards()<1){
-            deckOfCards.fillDeck();
-        }
-        for(int i=0;i<7;i++){
-            playerCards.add(deckOfCards.getCard());
-            dealerCards.add(deckOfCards.getCard());
+        this.goFishPlayer=player1;
+        this.dealer=dealer;
+        for (int i = 0; i < 7; i++) {
+                cardsAtHand.get(player1).add(deckOfCards.getCard());
+                cardsAtHand.get(dealer).add(deckOfCards.getCard());
         }
 
     }
-    public boolean ask(Card card){
+
+    public boolean ask(Card card) {
 
         return true;
     }
-    public void transfer(Card card,Player player){
+
+    public void transfer(Card card, ArrayList<Card>to,ArrayList<Card>from) {
+        while(from.contains(card)){
+            to.add(card);
+            from.remove(card);
+        }
 
     }
-    public void drawCard(Player player){
+
+    public void drawCard(Player player,Card card) {
+        cardsAtHand.get(player).add(card);
 
 
     }
-    public void passTurn(Player player1,Player player2){
+
+    public void passTurn(Player player1, Player player2) {
+    }
+
+    protected boolean add(Player player, Card card) {
+        if (hasEntry(player)) {
+            getCards(player).add(card);
+            return true;
+
+        } else {
+            ArrayList<Card> playerCards = new ArrayList<>();
+            playerCards.add(card);
+            cardsAtHand.put(player, playerCards);
+        }
+
+        return true;
+    }
+    protected boolean hasEntry(Player player){
+        if(cardsAtHand.containsKey(player)){
+            return true;
+        }
+        return false;
+    }
+    protected ArrayList<Card> getCards(Player player){
+        return cardsAtHand.get(player);
     }
 
 
--- src/test/java/io/zipcoder/casinotest/test/GameTest/cardGameTest/GoFishTest/GoFishTest.java
+++ src/test/java/io/zipcoder/casinotest/test/GameTest/cardGameTest/GoFishTest/GoFishTest.java
@@ -1,18 +1,55 @@
 package io.zipcoder.casinotest.test.GameTest.cardGameTest.GoFishTest;
 
+import io.zipcoder.casino.Game.cardGame.GoFIsh.GoFish;
+import io.zipcoder.casino.Game.cardGame.GoFIsh.GoFishPlayer;
 import io.zipcoder.casino.Game.cardGame.utilities.Card;
+import io.zipcoder.casino.Game.cardGame.utilities.CardRank;
+import io.zipcoder.casino.Game.cardGame.utilities.CardSuit;
+import io.zipcoder.casino.Game.cardGame.utilities.Deck;
 import io.zipcoder.casino.Player;
+import io.zipcoder.casino.Profile;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class GoFishTest {
     Player player1;
     Player dealer;
+    Deck testDeck = new Deck();
+
 
 
 
     @Test
     public void testDeal() {
+        Map<Player, ArrayList<Card>>testCardsAtHand = new HashMap<>();
+        ArrayList<Card> testDealerCards= new ArrayList<Card>();
+        ArrayList<Card>testPlayerCards= new ArrayList<Card>();
+        Deck testDeck = new Deck();
+        GoFish testGoFish= new GoFish();
+
+        System.out.println(testDeck.countRemainingCards());
+
+        Profile testProfilePlayer = new Profile("John",5000,0);
+        Profile testProfileDealer= new Profile("Mark",5000,1);
+        Player testPlayer = new GoFishPlayer(testProfilePlayer);
+        Player testDealer = new GoFishPlayer(testProfileDealer);
+
+        testCardsAtHand.put(testPlayer,testPlayerCards);
+        testCardsAtHand.put(testDealer,testDealerCards);
+        testGoFish.deal(testDeck,testPlayer,testDealer);
+        Integer expected = 7;
+        Integer actual = testCardsAtHand.get(testPlayer).size();
+
+        Assert.assertEquals(expected,actual);
+
+
+
+
 
 
     }
@@ -20,12 +57,26 @@ public class GoFishTest {
     @Test
     public void testAsk(){
 
+
     }
     @Test
     public void testTransfer(){
 
+        ArrayList<Card>testTo = new ArrayList<>();
+        ArrayList<Card>testFrom=new ArrayList<>();
+        Card card = new Card(CardSuit.DIAMONDS,CardRank.TWO);
+        testFrom.add(card);
+        int expected = 1;
+
+        GoFish testGoFish = new GoFish();
+        testGoFish.transfer(card,testTo,testFrom);
+
+        int actual = testTo.size();
+        Assert.assertEquals(expected,actual);
+
+
     }
-    @Test
+   /* @Test
     public void testDrawCard(Player player){
 
 
@@ -33,6 +84,6 @@ public class GoFishTest {
     @Test
     public void testPassTurn(Player player1,Player player2){
 
-    }
+    }*/
 
 }
