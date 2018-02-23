f1131b191a55e9b616a86ebf77e70147083336c7
Patrick Glavin <patjglavin@gmail.com>
2018-02-22 19:29:33 -0500

    whoops lets put this in blackjack

--- src/main/java/io/zipcoder/casino/Players/BlackjackPlayer.java
+++ src/main/java/io/zipcoder/casino/Players/BlackjackPlayer.java
@@ -1,8 +1,12 @@
 package io.zipcoder.casino.Players;
 
+import io.zipcoder.casino.GameTools.Deck.Card;
+
+import java.util.ArrayList;
+
 public class BlackjackPlayer extends Player{
     protected Player rootPlayer;
-    //private Card[] hand;
+    protected ArrayList<Card> hand;
     protected boolean canHit;
 
     public BlackjackPlayer(Player rootPlayer){
@@ -10,5 +14,26 @@ public class BlackjackPlayer extends Player{
         this.age = rootPlayer.getAge();
         this.canHit = true;
         this.rootPlayer = rootPlayer;
+        this.hand = new ArrayList<Card>();
+    }
+
+    public Player getRootPlayer() {
+        return rootPlayer;
+    }
+
+    public void setHand(ArrayList<Card> hand){
+        this.hand = hand;
+    }
+
+    public void addToHand(Card card){
+        this.hand.add(card);
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public boolean isCanHit() {
+        return canHit;
     }
 }
--- src/test/java/io/zipcoder/casino/Games/BlackjackTest.java
+++ src/test/java/io/zipcoder/casino/Games/BlackjackTest.java
@@ -1,8 +1,58 @@
 package io.zipcoder.casino.Games;
 
+import io.zipcoder.casino.GameTools.Deck.Card;
+import io.zipcoder.casino.GameTools.Deck.Rank;
+import io.zipcoder.casino.GameTools.Deck.Suit;
+import io.zipcoder.casino.Players.BlackjackPlayer;
+import io.zipcoder.casino.Players.Player;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BlackjackTest {
+
+    Player sue = new Player("sue", 30, 1000);
+    BlackjackPlayer sueBKJK;
+    Blackjack game;
+
+    @Before
+    public void setup(){
+        sueBKJK = new BlackjackPlayer(sue);
+        game = new Blackjack(sue);
+    }
+
+    @Test
+    public void constructorTest(){
+        String actual = game.playerList.get(0).getName();
+        String expected = sue.getName();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void dealTest(){
+        Card expected = game.deck.deck.get(0);
+        game.deal(sueBKJK);
+        Card actual = sueBKJK.getHand().get(0);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void initialHandTest(){
+        Card expected = game.deck.deck.get(1);
+        game.initialHand(sueBKJK);
+        Card actual = sueBKJK.getHand().get(1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void initialHandWithMultiplePlayersTest(){
+        Player jim = new Player("jim", 38, 300);
+        BlackjackPlayer jimBKJK = new BlackjackPlayer(jim);
+        Card expected = game.deck.deck.get(3);
+        game.initialHand(sueBKJK, jimBKJK);
+        Card actual = jimBKJK.getHand().get(1);
+        Assert.assertEquals(expected, actual);
+    }
 
     @Test
     public void startTest() {
