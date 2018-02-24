e8b2c8994f2717b35bef87a558ffb99712b8a13a
Joshua Wurdemann <joshuawurdemann@zipcoders-MacBook-Pro-9.local>
2018-02-24 17:10:10 -0500

    updated Black Jack Methods Hit ShowDealersFaceCard and CurrentScore

--- src/main/java/io/zipcoder/casino/Game/cardGame/BLackJack/BlackJackGame.java
+++ src/main/java/io/zipcoder/casino/Game/cardGame/BLackJack/BlackJackGame.java
@@ -1,29 +1,25 @@
 package io.zipcoder.casino.Game.cardGame.BLackJack;
 
 import io.zipcoder.casino.CasinoUtilities.Console;
-import io.zipcoder.casino.Game.Game;
 import io.zipcoder.casino.Game.cardGame.CardGame;
-import io.zipcoder.casino.Game.cardGame.utilities.Deck;
+import io.zipcoder.casino.Game.cardGame.utilities.Card;
 import io.zipcoder.casino.Game.cardGame.utilities.Hand;
 
 import io.zipcoder.casino.Player;
 import io.zipcoder.casino.Profile;
-
-import java.util.ArrayList;
 
 public class BlackJackGame extends CardGame {
     BlackJackPlayer player;
     BlackJackPlayer dealer;
 
 
+    public BlackJackGame(Profile profile) {
+        player = new BlackJackPlayer(profile);
+        this.addPlayer(player);
+        dealer = new BlackJackPlayer(null);
+        this.addPlayer(dealer);
 
-        public BlackJackGame(Profile profile){
-            player = new BlackJackPlayer(profile);
-            this.addPlayer(player);
-            dealer = new BlackJackPlayer(null);
-            this.addPlayer(dealer);
-
-        }
+    }
 
 
     public static void main(String[] args) {
@@ -40,26 +36,58 @@ public class BlackJackGame extends CardGame {
             // print card value to string
             dealer.getHand().addCard(deck.getCard());
         }
-        // prints player hand
 
         Console.print(player.getHand().showHand());
+
+    }
+
+    public void turn() {
+
     }
 
 
-    public void hit(Player aPlayer) {
+    public void hit(BlackJackPlayer aPlayer) {
+        Card cardToAdd = deck.getCard();
+        aPlayer.getHand().addCard(cardToAdd);
+        currentScore(cardToAdd, aPlayer);
+         // Console.print();
 
+        // if player score is over 21 change is busted to true
     }
 
     public void stand() {
 
     }
 
-    public void round() {
+    public int currentScore(Card cardToScore, BlackJackPlayer aPlayer) {
+        int cardValue = cardToScore.getRank().getCardValue();
+        int updateScore = aPlayer.getScore() + cardValue;
+        aPlayer.setScore(updateScore);
+        return updateScore;
+    }
+
+    public void round(Card card) {
 
     }
 
     public void dealerBehavior() {
 
+    }
+
+    public String ShowDealersFaceCard() {
+        Hand hand = dealer.getHand();
+        Card faceCard = hand.getCards().get(0);
+
+        // /Console.print(faceCard.toString());
+        return faceCard.toString();
+    }
+
+    public String showListOfPlayerActions() {
+        String playerActions = "Choose Action: [Bet], [Hit], [Stand], [Spilt]";
+
+        Console.print(playerActions);
+
+        return playerActions;
     }
 
 
--- src/test/java/io/zipcoder/casinotest/test/GameTest/cardGameTest/BlackJackTest/BlackJackGameTest.java
+++ src/test/java/io/zipcoder/casinotest/test/GameTest/cardGameTest/BlackJackTest/BlackJackGameTest.java
@@ -2,36 +2,67 @@ package io.zipcoder.casinotest.test.GameTest.cardGameTest.BlackJackTest;
 
 import io.zipcoder.casino.Game.cardGame.BLackJack.BlackJackGame;
 import io.zipcoder.casino.Game.cardGame.BLackJack.BlackJackPlayer;
+import io.zipcoder.casino.Game.cardGame.utilities.Card;
+import io.zipcoder.casino.Game.cardGame.utilities.Deck;
+import io.zipcoder.casino.Game.cardGame.utilities.Hand;
 import io.zipcoder.casino.Profile;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class BlackJackGameTest {
-
-    private Profile   testPlayerProfile;
-    private Profile   testDealerProfile;
+    private Deck testDeck;
+    private Hand testHand;
+    private Profile testPlayerProfile;
     private BlackJackPlayer testPlayer;
     private BlackJackPlayer testDealer;
     private BlackJackGame testBlackJackGAme;
+
     @Before
     public void setUp() throws Exception {
+        testDeck = new Deck();
+        testHand = new Hand();
+
         testPlayerProfile = new Profile("name", 1000.0, 1);
         testBlackJackGAme = new BlackJackGame(testPlayerProfile);
         testPlayer = new BlackJackPlayer(testPlayerProfile);
+        testDealer = new BlackJackPlayer(null);
+        testDeck = testBlackJackGAme.getDeck();
     }
 
     @Test
     public void dealTest() {
         testBlackJackGAme.deal();
         int expected = 48;
-        int actual =testBlackJackGAme.getDeck().countRemainingCards();
+        int actual = testBlackJackGAme.getDeck().countRemainingCards();
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void hitTest() {
+        // Card card1 = testDeck.getCard();
+        //testPlayer.getHand().addCard(card1);
+        //int score =   testPlayer.getScore();
+        testBlackJackGAme.hit(testPlayer);
+        int expected = 1;
+        int actual = testPlayer.getHand().getCards().size();
+
         Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void hitTest() {
+    public void currentScoreTest() {
+        Card cardToScore = testDeck.getCard();
+        Card card1 = testDeck.getCard();
+        testPlayer.getHand().addCard(card1);
 
+        testBlackJackGAme.hit(testPlayer);
+
+        int expected = card1.getRank().getCardValue() + cardToScore.getRank().getCardValue();
+        int actual = testBlackJackGAme.currentScore(cardToScore, testPlayer);
+
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
@@ -49,15 +80,26 @@ public class BlackJackGameTest {
 
     }
 
+    // need to get find way to fill expected String
     @Test
-    public void addPlayerTest() {
+    public void showDealerTopCardTEst() {
+        testBlackJackGAme.deal();
 
+
+        String expected = ""; //testDealer.getHand().getCards().get(1).toString();
+        String actual = testBlackJackGAme.ShowDealersFaceCard();
+
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
-    public void removePlayerTest() {
+    public void showListOfPlayerActionsTest() {
+        String expected = "Choose Action: [Bet], [Hit], [Stand], [Spilt]";
+        String actual = testBlackJackGAme.showListOfPlayerActions();
 
+        Assert.assertEquals(expected, actual);
     }
+
 
     @Test
     public void startGame() {
